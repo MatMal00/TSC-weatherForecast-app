0794e125424d67881209b5cbe02a70ec1f05ae52
Mateusz Malec <m.mateusz2000@wp.pl>
2021-08-23 11:19:45 +0200

    feat: add simple form

--- src/Weatchercast.tsx
+++ src/Weatchercast.tsx
@@ -1,7 +1,16 @@
-import './Weatchercast.css';
+import Form from '@components/form/Form';
+
+import styles from './Weatchercast.module.css';
 
 const Weathercast = () => {
-    return <h1>Weathercast</h1>;
+    return (
+        <div className={styles.container}>
+            <h1>Weathercast</h1>
+            <div className={styles.center}>
+                <Form />
+            </div>
+        </div>
+    );
 };
 
 export default Weathercast;
